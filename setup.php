41cb6993a92cb2787e3ceb10772dfe2a16e30ead
Richard Loureiro <richard.infoo@gmail.com>
2026-06-07 21:59:43 -0300

    mirror: sync whitelisted code

--- setup.php
+++ setup.php
@@ -22,7 +22,7 @@ if (!defined('GLPI_ROOT')) {
 require_once __DIR__ . '/inc/modulespath.inc.php';
 
 /** Versão do plugin (usada em plugin_version_nextool e migrations) */
-define('PLUGIN_NEXTOOL_VERSION', '4.2.0');
+define('PLUGIN_NEXTOOL_VERSION', '4.2.1');
 
 /** GLPI mínimo e máximo suportados (requisitos oficiais Teclib/marketplace) */
 define('PLUGIN_NEXTOOL_MIN_GLPI_VERSION', '11.0.0');
@@ -146,6 +146,11 @@ function plugin_init_nextool() {
    }
 
    $PLUGIN_HOOKS['csrf_compliant']['nextool'] = true;
+
+   // CSS global escopado a .nextool-tab-card: oculta os controles de "pesquisa salva"
+   // (SavedSearch) nas grades Search::show embarcadas em abas de modulo (bugados fora de
+   // pagina de busca pura) e remove a 2a barra de rolagem. Nao afeta telas puras.
+   $PLUGIN_HOOKS['add_css']['nextool'][] = 'front/nextool-tabs.css.php';
 
    try {
    Plugin::loadLang('nextool');
